c930f5913f80d285b8b60045342680bcf87a740b
Jiravat Boonkumnerd <jo06942@gmail.com>
2018-07-21 04:07:17 +0700

    Added Model nofeature

--- models/lightfm/model_no_feature.py
+++ models/lightfm/model_no_feature.py
@@ -1,26 +1,40 @@
 import pandas as pd
 import numpy as np
 
-
 import sys
 import tqdm
+import pickle
 sys.path.append('./')
 from features import features
+from metrics import average_precision
 
 import time
 import matplotlib
 import matplotlib.pyplot as plt
 from lightfm.evaluation import precision_at_k
 
-train = pd.read_csv('./input/train_20000.csv')
-test = pd.read_csv('./input/test_20000.csv')
+is_test=1
+if is_test:
+    train = pd.read_csv('./input/train_20000.csv')
+    test = pd.read_csv('./input/test_20000.csv')
+else:
+    train = pd.read_csv('./input/userLog_201801_201802_for_participants.csv', delimiter=';')
+    test = pd.read_csv('./input/testing_users.csv', delimiter=';')
 
-gp, interaction_col_name = features.count_duplicate(train, group_cols=['userCode', 'project_id'])
+print('train', train.shape)
+
+# drop duplicate
 train = train.drop_duplicates(['userCode','project_id'],keep='last')
-train = train.merge(gp, on=['userCode', 'project_id'], how='left');del gp
+
+# drop train userCode not in test UserCode todo
+# unique_test = test['userCode'].unique()
+# train = train[train['userCode'].isin(unique_test)]
+
+print('train', train.shape)
 
 from lightfm.data import Dataset
 from lightfm import LightFM
+
 # build dataset
 dataset = Dataset()
 
@@ -39,5 +53,100 @@ dataset.fit(users=user_iterable,
 num_users, num_items = dataset.interactions_shape()
 print('Num users: {}, num_items: {}.'.format(num_users, num_items))
 
-(train_interactions, weights) = dataset.build_interactions(data=((row['userCode'], row['project_id'], row[interaction_col_name])for index, row in train.iterrows()))
-(test_interactions, _) = dataset.build_interactions(data=((row['userCode'], row['project_id'])for index, row in test.iterrows()))
+(train_interactions, _) = dataset.build_interactions(data=((row['userCode'], row['project_id'])for index, row in train.iterrows()))
+if is_test:
+    (test_interactions, _) = dataset.build_interactions(data=((row['userCode'], row['project_id'])for index, row in test.iterrows()))
+
+# create Model
+epochs = 70 # 60
+alpha=0.001
+num_components=128
+schedule='adagrad'
+
+warp_model = LightFM(no_components=num_components,
+                            loss='warp',
+                            learning_schedule=schedule,
+                            user_alpha=alpha,
+                            item_alpha=alpha,
+                            random_state=44)
+
+warp_duration = []
+warp_pre = []
+
+for epoch in range(epochs):
+    start = time.time()
+    warp_model.fit_partial(train_interactions, epochs=1, num_threads=8)
+    time_=time.time() - start
+    warp_duration.append(time_)
+    if is_test:
+        precission=precision_at_k(warp_model, test_interactions, train_interactions=train_interactions, k=7).mean()
+    else:
+        precission=precision_at_k(warp_model, train_interactions, k=7).mean()
+    warp_pre.append(precission)
+    print('Fit Model Finish Epoch: {} ACC: {} TIME {}:'.format(epoch, precission, time_))
+    # save model checkpoint
+    if not is_test:
+        with open('warp_model_{}_{}.pickle'.format(epoch, precission), 'wb') as file_:
+            pickle.dump(warp_model, file_, protocol=pickle.HIGHEST_PROTOCOL)
+
+# predict
+is_predict = 1
+if is_predict:
+    num_project = len(unique_project)
+    unique_user_list = unique_user.tolist()
+    unique_project_list = unique_project.tolist()
+
+    # create visited dict
+    visited_dict = train.groupby('userCode')['project_id'].apply(lambda x: list(x)).to_dict()
+
+    predicted_list = []
+
+    with tqdm.tqdm(total=len(test)) as progress:
+        for uid in test['userCode'].unique():
+            predictions = warp_model.predict(unique_user_list.index(uid),
+                                    np.arange(num_project),
+                                    # user_features=user_feature_matrix,
+                                    # item_features=item_feature_matrix
+                                    )
+            top_items = unique_project.iloc[np.argsort(-predictions)]
+            top_list = []
+            top_n = 0
+            for project_id in top_items.values:
+                if project_id not in visited_dict[uid]: # todo add ignore project
+                    top_list.append(project_id)
+                    top_n+=1
+                if top_n >= 7:
+                    break
+            predicted_list.append(top_list)
+            progress.update(1)
+
+    to_csv = 1
+    if to_csv:
+        test['project_id'] = [' '.join(map(str, pre)) for pre in predicted_list]
+        csv_file_name = 'submission_{}_{}_{}_no_feature_no_clean_no_drop_usercode'.format(num_components, alpha, schedule)
+        test[['userCode','project_id']].to_csv('{}.csv'.format(csv_file_name), index=False)
+
+    if is_test:
+        actual_list = [[pid] for pid in test['project_id'].values]
+        print('%.10f'%average_precision.mapk(actual_list, predicted_list, k=7))
+
+# plot graph
+x = np.arange(epochs)
+plt.plot(x, np.array(warp_pre))
+plt.legend(['WARP P@K'], loc='upper right')
+eval_name = 'eval_{}_{}_{}_warp_no_feature_no_clean'.format(num_components, alpha, schedule)
+plt.savefig('{}.png'.format(eval_name))
+plt.clf()
+plt.cla()
+eval_df = pd.DataFrame({'WARP_PAK': warp_pre})
+eval_df.to_csv('{}.csv'.format(eval_name), index=False)
+
+x = np.arange(epochs)
+plt.plot(x, np.array(warp_duration))
+plt.legend(['WARP duration'], loc='upper right')
+time_name = 'time_{}_{}_{}_warp_no_feature_no_clean'.format(num_components, alpha, schedule)
+plt.savefig('{}.png'.format(time_name))
+plt.clf()
+plt.cla()
+time_df = pd.DataFrame({'WARP_TIME': warp_duration})
+time_df.to_csv('{}.csv'.format(time_name), index=False)
--- models/lightfm/optimise.py
+++ models/lightfm/optimise.py
@@ -15,12 +15,20 @@ from lightfm.evaluation import precision_at_k
 train = pd.read_csv('./input/train_20000.csv')
 test = pd.read_csv('./input/test_20000.csv')
 
+print('train', train.shape)
+
+# drop train userCode not in test UserCode
+unique_test = test['userCode'].unique()
+train = train[train['userCode'].isin(unique_test)]
+
+print('train', train.shape)
+
 from lightfm.data import Dataset
 from lightfm import LightFM
 # build dataset
 dataset = Dataset()
 
-unique_project = train['project_id'].drop_duplicates() # todo
+unique_project = train['project_id'].drop_duplicates()
 unique_user = train['userCode'].drop_duplicates()
 
 user_iterable = (row for row in unique_user)
@@ -38,70 +46,49 @@ print('Num users: {}, num_items: {}.'.format(num_users, num_items))
 (train_interactions, _) = dataset.build_interactions(data=((row['userCode'], row['project_id'])for index, row in train.iterrows()))
 (test_interactions, _) = dataset.build_interactions(data=((row['userCode'], row['project_id'])for index, row in test.iterrows()))
 
-# warp > bpr > log
+# for model bpr is not good so we drop first
 
 # the winner is warp, adagrade , more component 128 fast 256 slow is best maybe, more epoch more than 20 , 40-50 is best
 # no sample_weight is better than have
-# alpha 0.001 is best
 
 for num_components in [128]:# log is not about num_components and epoch [32, 64, 128, 256]
-    for schedule in ['adagrad']: # adagrad is better than adadelta ['adagrad','adadelta']
-        # num_components = 32
-        epochs = 70
-        # schedule = 'adadelta'
-        alpha=0.001
+    # num_components = 32
+    epochs = 50
+    schedule = 'adagrad'  # adagrad is better than adadelta
+    alpha=0.001 #0.001
 
-        log_model = LightFM(no_components=num_components,
-                            loss='logistic',
-                            learning_schedule=schedule,
-                            user_alpha=alpha,
-                            item_alpha=alpha)
+    warp_model = LightFM(no_components=num_components,
+                        loss='warp',
+                        learning_schedule=schedule,
+                        user_alpha=alpha,
+                        item_alpha=alpha,
+                        random_state=44)
 
-        warp_model = LightFM(no_components=num_components,
-                            loss='warp',
-                            learning_schedule=schedule,
-                            user_alpha=alpha,
-                            item_alpha=alpha)
+    warp_duration = []
+    warp_auc = []
 
-        warp_duration = []
-        log_duration = []
-        warp_auc = []
-        log_auc = []
+    for epoch in range(epochs):
+        start = time.time()
+        warp_model.fit_partial(train_interactions, epochs=1, num_threads=8)
+        warp_duration.append(time.time() - start)
+        warp_auc.append(precision_at_k(warp_model, test_interactions, train_interactions=train_interactions, k=7).mean())
 
-        for epoch in range(epochs):
-            start = time.time()
-            log_model.fit_partial(train_interactions, epochs=1, num_threads=8)
-            log_duration.append(time.time() - start)
-            log_auc.append(precision_at_k(log_model, test_interactions, train_interactions=train_interactions, k=7).mean())
+    x = np.arange(epochs)
+    plt.plot(x, np.array(warp_auc))
+    plt.legend(['WARP P@K'], loc='upper right')
+    eval_name = 'eval_{}_{}_{}_warp_drop_train'.format(num_components, alpha, schedule)
+    plt.savefig('{}.png'.format(eval_name))
+    plt.clf()
+    plt.cla()
+    eval_df = pd.DataFrame({'WARP_PAK': warp_auc})
+    eval_df.to_csv('{}.csv'.format(eval_name), index=False)
 
-        for epoch in range(epochs):
-            start = time.time()
-            warp_model.fit_partial(train_interactions, epochs=1, num_threads=8)
-            warp_duration.append(time.time() - start)
-            warp_auc.append(precision_at_k(warp_model, test_interactions, train_interactions=train_interactions, k=7).mean())
-
-        x = np.arange(epochs)
-        plt.plot(x, np.array(warp_auc))
-        plt.plot(x, np.array(log_auc))
-        plt.legend(['WARP P@K', 'LOG P@K'], loc='upper right')
-        eval_name = 'eval_{}_{}_{}'.format(num_components, alpha, schedule)
-        plt.savefig('{}.png'.format(eval_name))
-        plt.clf()
-        plt.cla()
-        eval_df = pd.DataFrame({'WARP_PAK': warp_auc,
-            'LOG_PAK': log_auc
-        })
-        eval_df.to_csv('{}.csv'.format(eval_name), index=False)
-
-        x = np.arange(epochs)
-        plt.plot(x, np.array(warp_duration))
-        plt.plot(x, np.array(log_duration))
-        plt.legend(['WARP duration', 'log duration'], loc='upper right')
-        time_name = 'time_{}_{}_{}'.format(num_components, alpha, schedule)
-        plt.savefig('{}.png'.format(time_name))
-        plt.clf()
-        plt.cla()
-        time_df = pd.DataFrame({'WARP_TIME': warp_duration,
-            'LOG_TIME': log_duration
-        })
-        time_df.to_csv('{}.csv'.format(time_name), index=False)
+    x = np.arange(epochs)
+    plt.plot(x, np.array(warp_duration))
+    plt.legend(['WARP duration'], loc='upper right')
+    time_name = 'time_{}_{}_{}_warp_drop_train'.format(num_components, alpha, schedule)
+    plt.savefig('{}.png'.format(time_name))
+    plt.clf()
+    plt.cla()
+    time_df = pd.DataFrame({'WARP_TIME': warp_duration})
+    time_df.to_csv('{}.csv'.format(time_name), index=False)
